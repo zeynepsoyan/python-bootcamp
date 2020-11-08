edb808d9d911aacf394a932e92b7a6a9bd6e7128
zeynepsoyan <zeynepsoyan@gmail.com>
2020-11-08 12:36:39 +0300

    day 1 done

--- day-1/lecture-notes.py
+++ day-1/lecture-notes.py
@@ -1,7 +1,3 @@
 #Take inputs from the user "inline"
 
 print("Hello " + input("1. What is your name? ") + "!\n")
-
-#So many functions you can write in one line -> Pythonic
-
-print("Your name has " + str(len(input("2. What is your name? "))) + " characters\n")
