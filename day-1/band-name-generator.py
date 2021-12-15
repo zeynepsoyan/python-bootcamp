145bb25588a7f81c817cf964da6ff8595ea5317e
zeynepsoyan <zeynepsoyan@gmail.com>
2021-12-15 22:51:54 +0300

    restart

--- day-1/band-name-generator.py
+++
@@ -1,8 +0,0 @@
-print('Welcome to the Band Name Generator!')
-print('Our aim is to create the best name for your rock band, yay!\n')
-
-city_name = input('What is the name of the city you grew up in?\n')
-pet_name = input('What is your first pet\'s name?\n')
-
-print('\nGenerating...\n')
-print('The best name for your band is... "' + city_name + ' ' + pet_name + '"')
