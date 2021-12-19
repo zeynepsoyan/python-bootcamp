6785209c318ab9f236ff0ce0fee24298b17b6cd8
zeynepsoyan <zeynepsoyan@gmail.com>
2021-12-19 13:12:41 +0300

    day 4 done

--- day-4/rock-paper-scissors.py
+++ day-4/rock-paper-scissors.py
@@ -32,16 +32,17 @@ print("Welcome to the Rock, Paper Scissors game. You are playing against a compu
 user_choice = int(input("So which will you choose? Rock (0), Paper (1) or Scissors (2)? "))
 comp_choice = random.choice([0,1,2])
 
-print("You chose: \n", hand[user_choice])
-print("Computer chose: \n", hand[comp_choice])
-
 if user_choice > 2 or user_choice < 0:
     print("You typed an invalid number")
-elif user_choice == comp_choice:
-    print("It's a draw")
-elif user_choice == 0 and comp_choice == 1 or user_choice == 1 and comp_choice == 2 or user_choice == 2 and comp_choice == 1:
-    print("You lose")
 else:
-    print("You win")
+    print("You chose: \n", hand[user_choice])
+    print("Computer chose: \n", hand[comp_choice])
+    
+    if user_choice == comp_choice:
+        print("It's a draw")
+    elif user_choice == 0 and comp_choice == 1 or user_choice == 1 and comp_choice == 2 or user_choice == 2 and comp_choice == 1:
+        print("You lose")
+    else:
+        print("You win")
 
 
