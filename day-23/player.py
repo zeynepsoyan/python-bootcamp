eeb12a36ef3727aab5f829ac7aece91f5adc1ceb
zeynepsoyan <zeynepsoyan@gmail.com>
2022-01-23 21:00:52 +0300

    day 23 done

---
+++ day-23/player.py
@@ -0,0 +1,23 @@
+from turtle import Turtle
+
+STARTING_POSITION = (0, -280)
+MOVE_DISTANCE = 10
+FINISH_LINE_Y = 280
+
+
+class Player(Turtle):
+    def __init__(self) -> None:
+        super().__init__()
+        self.shape("turtle")
+        self.penup()
+        self.go_to_start()
+        self.setheading(90)
+
+    def up(self):
+        self.forward(MOVE_DISTANCE)
+
+    def go_to_start(self):
+        self.goto(STARTING_POSITION)
+
+    def is_at_finish(self):
+        return self.ycor() >= FINISH_LINE_Y
